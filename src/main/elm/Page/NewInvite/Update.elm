3df9a991780780696b58f9b0d9677f6cdead4039
Eike Kettner <eike.kettner@posteo.de>
2020-05-24 09:56:43 +0200

    Remove uses of debug from elm source

--- src/main/elm/Page/NewInvite/Update.elm
+++ src/main/elm/Page/NewInvite/Update.elm
@@ -1,24 +1,35 @@
 module Page.NewInvite.Update exposing (update)
 
 import Api
-import Ports
-import Page.NewInvite.Data exposing (..)
 import Data.InvitePassword exposing (InvitePassword)
+import Page.NewInvite.Data exposing (..)
 
-update: Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SetPassword text ->
-            ({model|invpass = text}, Cmd.none)
+            ( { model | invpass = text }, Cmd.none )
 
         Generate ->
-            ({model|process = True}, Api.generateInvite model.flags (InvitePassword model.invpass) GenerateResp)
+            ( { model | process = True }
+            , Api.generateInvite model.flags (InvitePassword model.invpass) GenerateResp
+            )
 
         GenerateResp (Ok inv) ->
-            ({model|process = False, invite = Just inv, error = Nothing}, Cmd.none)
+            ( { model
+                | process = False
+                , invite = Just inv
+                , error = Nothing
+              }
+            , Cmd.none
+            )
 
-        GenerateResp (Err err) ->
-            let
-                _ = Debug.log "Error:" err
-            in
-                ({model|process = False, error = Just "Generating key failed", invite = Nothing}, Cmd.none)
+        GenerateResp (Err _) ->
+            ( { model
+                | process = False
+                , error = Just "Generating key failed"
+                , invite = Nothing
+              }
+            , Cmd.none
+            )
